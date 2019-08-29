cc91ce6fde75e562baa9987d6eef6f894e9dacb4
minatosan <yuuki3yuki3@yahoo.co.jp>
2019-08-29 15:17:15 +0900

    [WIP] create-incrimental seach

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,9 @@ class UsersController < ApplicationController
 
   def index
       @users=User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
-      #binding.pry
       respond_to do |format|
         format.html
         format.json
-        #binding.pry
       end
   
   end
